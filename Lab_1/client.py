38fa3342c9704fdbb92c2a7fdd922d4cc0a72c65
Krystian Ruczyński <81698619+krystian98r@users.noreply.github.com>
2021-03-31 17:27:35 +0200

    Add files via upload

--- Lab_1/client.py
+++ Lab_1/client.py
@@ -3,7 +3,7 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 20000)
+server_address = ('localhost', 12345)
 sock.connect(server_address)
 
 try:
