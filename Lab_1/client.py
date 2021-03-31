256991fb49bc3924bcda8ef71c3445cc20f735b0
Krystian Ruczyński <81698619+krystian98r@users.noreply.github.com>
2021-03-31 17:12:48 +0200

    Create client.py

---
+++ Lab_1/client.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('localhost', 20000)
+sock.connect(server_address)
+
+try:
+    message = 'This is the message...'
+    sock.sendall(message)
+    data = sock.recv(100)
+    print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    sock.close()
